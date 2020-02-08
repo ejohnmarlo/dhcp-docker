f53c3badf16c4670f6984a31418cf2f6a2e6b86e
John Marlo Evangelista <ejohnmarlo@gmail.com>
2020-02-08 23:25:45 +0800

    Update entrypoint.sh

--- util/entrypoint.sh
+++ util/entrypoint.sh
@@ -8,6 +8,9 @@ mv -f /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.backup
 mv -f util/isc-dhcp-server /etc/default/isc-dhcp-server
 mv -f util/dhcpd.conf /etc/dhcp/dhcpd.conf
 
+#Enable Promiscuous Mode in eth0
+ifconfig eth0 promisc
+
 #Delete initiated dhcpd pid, consult journalctl -xe | grep dhcp after restart isc-dhcp-server
 #rm /var/run/dhcpd.pid
 

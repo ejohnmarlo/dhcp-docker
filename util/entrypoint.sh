f8aacbcfaf3bffbdb1c902507c6ba0a7d364b9c0
John Marlo Evangelista <ejohnmarlo@gmail.com>
2020-02-08 15:47:42 +0800

    Update entrypoint.sh

--- util/entrypoint.sh
+++ util/entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
 touch sample.txt
+#Backup
+mv -f /etc/default/isc-dhcp-server /etc/default/isc-dhcp-server.backup
+mv -f /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.backup
+
+#Replace
+mv -f util/isc-dhcp-server /etc/default/isc-dhcp-server
+mv -f util/dhcpd.conf /etc/dhcp/dhcpd.conf
+
+systemctl enable isc-dhcp-server
+systemctl restart isc-dhcp-server
+
 tail -f /dev/null
